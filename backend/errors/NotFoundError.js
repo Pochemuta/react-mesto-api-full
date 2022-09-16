b6c981861118ce4c585540f545c1b59cafdece8f
pochemuta <pochemuta92@gmail.com>
2022-09-16 11:16:51 +0800

    testing project

--- backend/errors/NotFoundError.js
+++
@@ -1,8 +0,0 @@
-class NotFoundError extends Error {
-  constructor(message = 'NotFound') {
-    super(message);
-    this.statusCode = 404;
-  }
-}
-
-module.exports = NotFoundError;
--- frontend/src/components/reg-auth/InfoTooltip.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function InfoTooltip() {
-  return <section className="info-tool-tip"></section>;
-}
