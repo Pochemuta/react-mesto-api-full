6402f1a6a7648b5edfe44260cd7bc33f842da53c
Pochemuta <54738089+Pochemuta@users.noreply.github.com>
2022-09-16 16:47:35 +0800

    fix: changed auth middleware

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,17 +4,18 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const { JWT_SECRET = 'secret' } = process.env;
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
-  if (!token) {
-    next(new UnauthorizedError('Необходима авторизация'));
-  } else {
-    let payload;
-    try {
-      payload = jwt.verify(token, JWT_SECRET);
-    } catch (err) {
-      next(new UnauthorizedError('Необходима авторизация'));
-    }
-    req.user = payload;
-    next();
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    next(new UnauthorizedError('Нужна авторизация'));
   }
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    next(new UnauthorizedError('Необходима авторизация'));
+  }
+  req.user = payload;
+  next();
 };
